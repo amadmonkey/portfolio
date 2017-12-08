98c4efe7ccf4aec6fe71ca396a1ef7ec9178f294
arcenio <aquinoarcie@gmail.com>
2017-12-08 13:32:05 +0800

    First release

---
+++ data/images.js
@@ -0,0 +1,52 @@
+var images = [
+    {
+        'id': 'img_00',
+        'title': 'Logo',
+        'description': 'My very own custom logo. There\'s just something about monkeys...'
+    },
+    {
+        'id': 'img_01',
+        'title': 'Flamingo Mascot',
+        'description': 'Used for Flamingo Mobile Bar\'s logo.'
+    },
+    {
+        'id': 'img_02',
+        'title': 'Flamingo Mobile Bar\'s Logo(full) ',
+        'description': 'Full and final version of the logo.'
+    },
+    {
+        'id': 'img_03',
+        'title': 'Vector Style Drinks',
+        'description': 'Used for Flamingo Mobile Bar\'s drink menus, posters, etc.'
+    },
+    {
+        'id': 'img_04',
+        'title': 'Get to Work Logo',
+        'description': 'Used for my college thesis.'
+    },
+    {
+        'id': 'img_06',
+        'title': 'Get to Work Mascot',
+        'description': 'Used for my college thesis.'
+    },
+    {
+        'id': 'img_08',
+        'title': 'Project Green',
+        'description': 'Made for a friend\'s thesis.'
+    },
+    {
+        'id': 'img_09',
+        'title': 'Poster Contest Entry',
+        'description': 'Winning entry for a college graphics poster making contest.'
+    },
+    {
+        'id': 'img_11',
+        'title': 'Shirt Design',
+        'description': 'Made for my college class\'s custom shirt design, then later was used for the IT college\'s yearly shirt design.'
+    },
+    {
+        'id': 'img_12',
+        'title': 'Batman',
+        'description': 'I like Batman.'
+    }
+];
