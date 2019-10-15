5c8605d3214d1e87d607ad22b2624e2a741d47fc
Arcenio <aquinoarcie@gmail.com>
2019-10-15 19:50:15 +0800

    Updates

---
+++ precache-manifest.5cd636ccef4a9e0226ad86a07ba74361.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc063b25f5c9d8923811dc3349bd273a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e5448a1ebe5522e15b0",
+    "url": "/portfolio/static/css/main.013cb8d9.chunk.css"
+  },
+  {
+    "revision": "9f9170465d457b562386",
+    "url": "/portfolio/static/js/2.638d8c9b.chunk.js"
+  },
+  {
+    "revision": "8e5448a1ebe5522e15b0",
+    "url": "/portfolio/static/js/main.76042932.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "290d648d85c5bd479c77af1ccdc9922e",
+    "url": "/portfolio/static/media/CarbonBlock.290d648d.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/portfolio/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/portfolio/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "bcabafa3de53cc681d988acccf6e099b",
+    "url": "/portfolio/static/media/bars.bcabafa3.svg"
+  },
+  {
+    "revision": "bd99bf52a33c8fa4bc7017f2da5a97d5",
+    "url": "/portfolio/static/media/brackets.bd99bf52.svg"
+  },
+  {
+    "revision": "64b279286fb85a75ade14b922bbc56bf",
+    "url": "/portfolio/static/media/download.64b27928.svg"
+  },
+  {
+    "revision": "39ead709af4a225982d4e4ea7c76e0ed",
+    "url": "/portfolio/static/media/email.39ead709.svg"
+  },
+  {
+    "revision": "3b1bd245272aa9b71f321e3c0dce5556",
+    "url": "/portfolio/static/media/html.3b1bd245.svg"
+  },
+  {
+    "revision": "a921539c4446656b2fbb225d67a05bd2",
+    "url": "/portfolio/static/media/mm.a921539c.svg"
+  },
+  {
+    "revision": "264af2e29b986b586f0e736b8232205d",
+    "url": "/portfolio/static/media/pencil.264af2e2.svg"
+  },
+  {
+    "revision": "92dad3c37c817b82ac3d0292623d36ef",
+    "url": "/portfolio/static/media/phone.92dad3c3.svg"
+  },
+  {
+    "revision": "fd930dd663b86b86eb94ff957e8d2525",
+    "url": "/portfolio/static/media/ruler.fd930dd6.svg"
+  },
+  {
+    "revision": "5995ae464f1099ccf9a13abbc7ead191",
+    "url": "/portfolio/static/media/star.5995ae46.svg"
+  }
+]);
