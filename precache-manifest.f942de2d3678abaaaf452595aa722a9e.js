4fc16be760b8381c7cc8d904f38f786ed3eae307
Arcenio <aquinoarcie@gmail.com>
2020-01-26 04:20:53 +0800

    Updates

--- precache-manifest.f942de2d3678abaaaf452595aa722a9e.js
+++ precache-manifest.f942de2d3678abaaaf452595aa722a9e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ba21a6f5a0ef4cb09040163be06b289",
+    "revision": "f9aa7ca05b05324fce0d9793c099e4d4",
     "url": "/portfolio/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7abfcc8e83c7460ce66a516ea20d7828.js"
+  "/portfolio/precache-manifest.f942de2d3678abaaaf452595aa722a9e.js"
 );
 
 self.addEventListener('message', (event) => {
