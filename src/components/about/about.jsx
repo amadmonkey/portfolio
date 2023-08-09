e0433dae6d7d811ac12635aa57723bf61199f6b6
Arcie Aquino <aquinoarcie@gmail.com>
2023-08-09 12:14:46 +0800

    resume update

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,67 +6,66 @@ import ExperienceList from "../../data/experience.js";
 import "./about.scss";
 
 const About = () => {
-    return (
-        <section id="_ABOUT_SECTION" className="about">
-            <div className="content about-content">
-                <p>
-                    I do web development and I love doing it with style.
-                    I like thinking of new ways to improve processes and make it easier for users to understand and navigate through applications.
-                    Life's hard as it is why add to it? I love making applications fun to use.
-                    I love creating algorithms and just simply try and make things work.
-                </p>
-                <div className="about-container">
-                    <div>
-                        {/* EXPERIENCE */}
-                        <div className="about-item">
-                            <header>Experience</header>
-                            <div className="about-item-content">
-                                {ExperienceList.experience.map((value, index) => {
-                                    return (
-                                        <div className="experience-item" key={index}>
-                                            <h1 className="title">{value.name}</h1>
-                                            <h2>
-                                                <span className="location-icon"></span>{" "}
-                                                <span>{value.address}</span>
-                                            </h2>
-                                            <span className="position">{value.position}</span>
-                                            <span className="dates">{value.dates}</span>
-                                            <div className="tools-container">
-                                                <span className="tools-header">Tools used:</span>
-                                                <ul>
-                                                    {value.tools.map((value, index) => {
-                                                        return <li key={index}>{value}</li>;
-                                                    })}
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        {/* EDUCATION */}
-                        <div className="about-item">
-                            <header>Education</header>
-                            <div className="about-item-content education">
-                                <h1>Tertiary</h1>
-                                <h2>Cavite State University</h2>
-                                <h2>Imus Campus</h2>
-                                <h2>Bachelor of Science in Information Technology</h2>
-                                <h2>2011 - 2016</h2>
-                                <br />
-                                <h1>Secondary</h1>
-                                <h2>Saint Francis Institute</h2>
-                                <h2>2002 - 2006</h2>
-                                <br />
-                                <h1>Primary</h1>
-                                <h2>Oak Tree Christian Academy</h2>
-                                <h2>1996 - 2002</h2>
-                            </div>
-                        </div>
-                        {/* AWARDS */}
-                        {/* <div className="about-item">
+	return (
+		<section id="_ABOUT_SECTION" className="about">
+			<div className="content about-content">
+				<p>
+					I do web development and I love doing it with style. I like thinking of new ways to
+					improve processes and make it easier for users to understand and navigate through
+					applications. Life's hard as it is why add to it? I love making applications fun to use. I
+					love creating algorithms and just simply try and make things work.
+				</p>
+				<div className="about-container">
+					<div>
+						{/* EXPERIENCE */}
+						<div className="about-item">
+							<header>Experience</header>
+							<div className="about-item-content">
+								{ExperienceList.experience.map((value, index) => {
+									return (
+										<div className="experience-item" key={index}>
+											<h1 className="title">{value.name}</h1>
+											<h2>
+												<span className="location-icon"></span> <span>{value.address}</span>
+											</h2>
+											<span className="position">{value.position}</span>
+											<span className="dates">{value.dates}</span>
+											<div className="tools-container">
+												<span className="tools-header">Tools used:</span>
+												<ul>
+													{value.tools.map((value, index) => {
+														return <li key={index}>{value}</li>;
+													})}
+												</ul>
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+					<div>
+						{/* EDUCATION */}
+						<div className="about-item">
+							<header>Education</header>
+							<div className="about-item-content education">
+								<h1>Tertiary</h1>
+								<h2>Cavite State University</h2>
+								<h2>Imus Campus</h2>
+								<h2>Bachelor of Science in Information Technology</h2>
+								<h2>2011 - 2016</h2>
+								<br />
+								<h1>Secondary</h1>
+								<h2>Saint Francis Institute</h2>
+								<h2>2002 - 2006</h2>
+								<br />
+								<h1>Primary</h1>
+								<h2>Oak Tree Christian Academy</h2>
+								<h2>1996 - 2002</h2>
+							</div>
+						</div>
+						{/* AWARDS */}
+						{/* <div className="about-item">
                             <header>Awards</header>
                             <div className="about-item-content awards">
                                 <h1>Developer Bootcamp</h1>
@@ -90,36 +89,38 @@ const About = () => {
                                 <h2>Cavite State University - Imus Campus</h2>
                             </div>
                         </div> */}
-                        {/* CONTACT */}
-                        <div className="about-item">
-                            <header>Contact</header>
-                            <div className="about-item-content">
-                                <button className="btn btn-download">
-                                    <a href="https://drive.google.com/file/d/1MQwrK-2m9astr0gJMsMgmKECbfAToZG3/view?usp=sharing" rel="noopener noreferrer" target="_blank">
-                                        <Download className="download-icon" />{" "}
-                                        <span>DOWNLOAD RESUME</span>
-                                    </a>
-                                </button>
-                                <button className="btn btn-call">
-                                    <a href="tel:916-552-3960">
-                                        <Phone className="phone-icon" /> <span>09165523960</span>
-                                    </a>
-                                </button>
-                                <button className="btn btn-email">
-                                    <a href="mailto:aquinoarcie@gmail.com">
-                                        <Email className="email-icon" />{" "}
-                                        <span>SEND ME AN EMAIL</span>
-                                    </a>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="egg egg-yellow"></div>
-            <div className="egg egg-purple"></div>
-        </section>
-    );
-}
+						{/* CONTACT */}
+						<div className="about-item">
+							<header>Contact</header>
+							<div className="about-item-content">
+								<button className="btn btn-download">
+									<a
+										href="https://drive.google.com/file/d/1SbRnN4dX969MTkY1IPThamb_1K2Pz2ZA/view?usp=sharing"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<Download className="download-icon" /> <span>DOWNLOAD RESUME</span>
+									</a>
+								</button>
+								<button className="btn btn-call">
+									<a href="tel:916-552-3960">
+										<Phone className="phone-icon" /> <span>09165523960</span>
+									</a>
+								</button>
+								<button className="btn btn-email">
+									<a href="mailto:aquinoarcie@gmail.com">
+										<Email className="email-icon" /> <span>SEND ME AN EMAIL</span>
+									</a>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="egg egg-yellow"></div>
+			<div className="egg egg-purple"></div>
+		</section>
+	);
+};
 
 export default About;
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import MoveIndicator from '../../utilities/move-indicator/move-indicator';
-import { ReactComponent as Logo } from '../../img/mm.svg';
+import React from "react";
+import MoveIndicator from "../../utilities/move-indicator/move-indicator";
+import { ReactComponent as Logo } from "../../img/mm.svg";
 import { ReactComponent as Download } from "../../img/download-primary.svg";
-import './hero.scss';
+import "./hero.scss";
 
 const Hero = () => {
-    return (
-        <section id="_HERO_SECTION" className="hero">
-            <div className="logo-container">
-                <Logo className="logo" />
-            </div>
-            <div className="hero-description">
-                <h2>hiya!</h2>
-                <h1><span className="capitalize">I</span>'m <span className="capitalize">A</span>rcie</h1>
-                <p>I do web development and I love doing it with <span>style</span></p>
-                <button className="btn btn-lg btn-ghost">
-                    <a href="https://drive.google.com/file/d/1MQwrK-2m9astr0gJMsMgmKECbfAToZG3/view?usp=sharing" rel="noopener noreferrer" target="_blank">
-                        <Download className="download-icon" />
-                        <span>Get Resume</span>
-                    </a>
-                </button>
-                <MoveIndicator />
-            </div>
-            <div className="egg egg-yellow active"></div>
-            <div className="egg egg-purple active"></div>
-        </section>
-    );
-}
+	return (
+		<section id="_HERO_SECTION" className="hero">
+			<div className="logo-container">
+				<Logo className="logo" />
+			</div>
+			<div className="hero-description">
+				<h2>hiya!</h2>
+				<h1>
+					<span className="capitalize">I</span>'m <span className="capitalize">A</span>rcie
+				</h1>
+				<p>
+					I do web development and I love doing it with <span>style</span>
+				</p>
+				<button className="btn btn-lg btn-ghost">
+					<a
+						href="https://drive.google.com/file/d/1SbRnN4dX969MTkY1IPThamb_1K2Pz2ZA/view?usp=sharing"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						<Download className="download-icon" />
+						<span>Get Resume</span>
+					</a>
+				</button>
+				<MoveIndicator />
+			</div>
+			<div className="egg egg-yellow active"></div>
+			<div className="egg egg-purple active"></div>
+		</section>
+	);
+};
 
 export default Hero;
